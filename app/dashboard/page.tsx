0250b016a79e266005429fc614cf1f5317e0adbf
makfm160 <makfm1@gmail.com>
2024-02-02 12:19:28 +0100

    Dashboard javítása

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Card } from '@/app/ui/dashboard/cards'
+import RevenueChart from '../ui/dashboard/revenue-chart'
+import LatestInvoices from '../ui/dashboard/latest-invoices'
+import { lusitana } from '@/app/ui/fonts'
+
 export default function Page() {
-    return <p>Dashboard Page</p>
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard 5
+            </h1>
+        </main>
+    )
 }
